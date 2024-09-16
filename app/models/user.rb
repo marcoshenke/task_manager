c09f35c8a76ff06882746fdca64257b7ec8e7f1d
@marcos.henke <marcos.henke@dponet.com.br>
2024-09-15 22:00:56 -0300

    config cors sem o origins determinado

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   before_save :downcase_email
 
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
 
   private
 
