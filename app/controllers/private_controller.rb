1b405a4035091ee35966b35c30b5c82764217a18
marcoshenke <marcos.henke3@gmail.com>
2024-09-29 20:58:27 -0300

    configurando rubocop

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -3,7 +3,7 @@ class PrivateController < ApplicationController
 
   def test
     render json: {
-      message: "This is a secret message. You are seeing it because you have successfully logged in."
+      message: 'This is a secret message. You are seeing it because you have successfully logged in.'
     }
   end
 end
