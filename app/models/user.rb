584c7cb6be30d0382f47309dfc9555eb6019f24c
marcoshenke <marcos.henke3@gmail.com>
2024-09-29 21:29:14 -0300

    JwtDenylist

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
   before_save :downcase_email
 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+                    presence: true, uniqueness: true
 
   private
 
